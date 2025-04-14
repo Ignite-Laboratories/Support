d1c57b307d44c5d1309d7a8e1344154c7c823504
Robert Petz <rpetz@ignitelabs.net>
2025-04-14 15:19:26 -0700

    Cleaned up the module reporting

--- doc.go
+++ doc.go
@@ -1,4 +1,12 @@
 // Package support contains miscellaneous functions that are too general for a single project in JanOS.
 package support
 
+import "github.com/ignite-laboratories/core"
+
 var ModuleName = "support"
+
+func init() {
+	core.ModuleReport(ModuleName)
+}
+
+func Report() {}
