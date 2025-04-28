2bcdbb6a89bf7bfac544ca874702f2b2e7bbd246
Robert Petz <rpetz@ignitelabs.net>
2025-04-27 18:27:49 -0700

    Made getting Ipsum data a little more idiomatic

--- ipsum/ipsum.go
+++ ipsum/ipsum.go
@@ -5,15 +5,19 @@ package ipsum
 var Paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet libero eu neque facilisis, ac pretium nisi dignissim. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.\n"
 
 // Generate returns paragraphs of 'Lorem ipsum' text.
-func Generate(paragraphs ...int) []byte {
+func Generate(paragraphs ...int) string {
 	count := 1
 	if len(paragraphs) > 0 {
 		count = paragraphs[0]
 	}
 
-	output := Paragraph
+	var output string
 	for i := 0; i < count; i++ {
 		output += Paragraph
 	}
-	return []byte(output)
+	return output
+}
+
+func GenerateBytes(paragraphs ...int) []byte {
+	return []byte(Generate(paragraphs...))
 }
